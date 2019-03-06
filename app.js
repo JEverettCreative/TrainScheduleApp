bc6cc4e232930e4e494ab271c8ca197b62efc40b
JEverett <jonathan@jonathaneverettcreative.com>
2019-03-06 16:17:08 -0600

    Initialized pages and directories. Added cdns for Bootstrap, jquery and added script and config for Firebase sync.

---
+++ app.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    // Initialize firebase configuration
+    var config = {
+        apiKey: "AIzaSyCkvf-3gihIqhgt5PiqwAiLDauzlZg1918",
+        authDomain: "trainscheduleapp-73dc4.firebaseapp.com",
+        databaseURL: "https://trainscheduleapp-73dc4.firebaseio.com",
+        projectId: "trainscheduleapp-73dc4",
+        storageBucket: "trainscheduleapp-73dc4.appspot.com",
+        messagingSenderId: "151619763461"
+      };
+      firebase.initializeApp(config);
+
+
+
+
+});
