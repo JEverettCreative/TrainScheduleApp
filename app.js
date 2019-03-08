3c884342945539f186ad9906b8fa15084c9667f8
JEverett <jonathan@jonathaneverettcreative.com>
2019-03-07 21:21:37 -0600

    Incorporated moment to push user input time to database after being converted to unix.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// $(document).ready(function(){
+$(document).ready(function(){
   var config = {
       apiKey: "AIzaSyCkvf-3gihIqhgt5PiqwAiLDauzlZg1918",
       authDomain: "trainscheduleapp-73dc4.firebaseapp.com",
@@ -20,24 +20,46 @@
 
 // Add on click function to take data from input fields upon btn submission
 
-$("#add-train").on("click", function(event){
-    event.preventDefault();
+    $("#add-train").on("click", function(event){
+        event.preventDefault();
 
-    trainName = $("#train-name-input").val().trim();
-    destination = $("#train-dest-input").val().trim();
-    firstTrainTime = $("#train-start-input").val().trim();
-    frequency = $("#train-freq-input").val().trim();
+        trainName = $("#train-name-input").val().trim();
+        destination = $("#train-dest-input").val().trim();
+        firstTrainTime = $("#train-start-input").val().trim();
+        frequency = $("#train-freq-input").val().trim();
 
-    console.log(trainName, destination, firstTrainTime, frequency);
-// Push this data into the firebase as a child object of the root
-    database.ref().push({
-      trainName: trainName,
-      destination: destination,
-      firstTrainTime: firstTrainTime,
-      frequency: frequency
-      
-    });
+    // Convert incoming first train time input into unix. Set it back 1 year to ensure it doesn't conflict w/ current time
+        var firstTimeConverted = moment(firstTrainTime, "HH:mm");
+        firstTimeConverted = moment(firstTimeConverted).format("X");
+        
 
-})
 
-// });
+        console.log(trainName, destination, firstTimeConverted, frequency);
+    // Push this data into the firebase as a child object of the root
+        database.ref().push({
+          trainName: trainName,
+          destination: destination,
+          firstTimeConverted: firstTimeConverted,
+          frequency: frequency
+          
+        });
+
+    })
+
+// Create a snapshot function on the child elements of the root database
+    // To push data to the HTML every time a val changes
+      // database.ref().on("child_added", function(childSnapshot){
+
+      //   var cSVal = childSnapshot.val();
+      //   // Create a new row for the table
+      //   var newTableRow = $("<tr>");
+      //   // Create new data cells for each variable
+      //   var tableTrainName = $("<td>").
+
+
+      // }, function(errorObject){
+      //   console.log("Errors encountered: " + errorCode);
+      // })
+
+
+});
